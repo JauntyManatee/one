b369958053c6d1190986224a8fe59aade228cb9d
Brian Graham <graham.briank@gmail.com>
2015-10-14 16:24:16 -0700

    Add ability to request all static files

--- server/server.py
+++ server/server.py
@@ -4,7 +4,11 @@ app = Flask(__name__, static_folder='../client')
  
 @app.route('/')
 def home():
-  return app.send_from_directory('../client')
+  return app.send_static_file('index.html')
  
+@app.route('/<path:path>')
+def static_prox(path):
+  return app.send_static_file(path)
+
 if __name__ == '__main__':
   app.run(debug=True)
