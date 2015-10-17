df8b26c9ddfed39c2238ddfc5f5d66bf2f8401c1
Niraj Vora <nsvora@ucdavis.edu>
2015-10-17 11:20:56 -0700

    modified my enviornment to work with most recent pull

--- project/server.py
+++ project/server.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template 
+import sys
+sys.path.append('server_logic')
+
+from twitterRoutes import *
  
 app = Flask(__name__)      
 
 @app.route('/')
 def home():
   return render_template('index.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
 
 #not sure if we need this anymore...
 #@app.route('/<path:path>')
