8d8a40f581e3a53bfb63909ea2d6452087a357a8
Niraj Vora <nsvora@ucdavis.edu>
2015-10-19 14:32:42 -0700

    i did it

--- project/server.py
+++ project/server.py
@@ -12,9 +12,6 @@ app = Flask(__name__)
 def home():
   return render_template('index.html')
 
-if __name__ == '__main__':
-  app.run(debug=True)
-
 #not sure if we need this anymore...
 #@app.route('/<path:path>')
 #def seeStaticFile(path):
