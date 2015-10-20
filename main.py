89d11d5d789633c8392e9b19f6676e40778019df
Ryan Wholey <rjwholey@gmail.com>
2015-10-20 12:36:15 -0700

    added instagram.py, pulls ig feed @ instagram/feed

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ sys.path.append('project')
 
 from server import *
 
-from reddit import *
 from flask.ext.bower import Bower
-
+from reddit import *
+from instagram import *
 
 Bower(app)
 Reddit(app)
+Instagram(app)
 
 if __name__ == '__main__':
   app.run(debug=True) 
