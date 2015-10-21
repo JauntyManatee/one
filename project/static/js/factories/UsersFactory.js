c54e75bc37b8e49b65a5315fa0152ef8917fd9c8
sndsgn <forloine@gmail.com>
2015-10-21 14:31:44 -0700

    Remove commented out code.

--- project/static/js/factories/UsersFactory.js
+++ project/static/js/factories/UsersFactory.js
@@ -2,15 +2,6 @@ app.factory('UsersFactory', ['$http',
   function($http, $location, $window) {
     return {
 
-//      checkUserExists: function(user) {
-//       console.log('factory');
-//        return $http({
-//          method: 'POST',
-//          url: '/usercheck',
-//          data: user
-//        });
-//      },
-
       signup: function(user) {
         return $http({
           method: 'POST',
