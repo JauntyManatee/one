1a410a4bb6973e0bc62dd41d889b174f52de258c
Brian Graham <graham.briank@gmail.com>
2015-10-22 19:06:50 -0700

    issues

--- project/static/js/factories/InstagramFactory.js
+++ project/static/js/factories/InstagramFactory.js
@@ -10,23 +10,24 @@ app.factory('InstagramFactory', ['$http', function ( $http ) {
   return {
     getInstaFeed: getInstaFeed
   };
-}]);
-//   .filter('instaLinky',['$filter', function($filter) {
-//     return function(text, target) {
-//       if (!text) return text;
+}])
+  .filter('instaLinky',['$filter', function($filter) {
+    return function(text, target) {
+      if (!text) return text;
 
-//       var replacedText = $filter('linky')(text, target);
-//       var targetAttr = "";
-//       if (angular.isDefined(target)) {
-//           targetAttr = ' target="' + target + '"';
-//       }
-//       // replace #hashtags and send them to twitter
-//       var replacePattern1 = /(^|\s)#(\w*[a-zA-Z_]+\w*)/gim;
-//       replacedText = text.replace(replacePattern1, '$1<a href="https://instagram.com/explore/tags/$2"' + targetAttr + '>#$2</a>');
-//       // replace @mentions but keep them to our site
-//       var replacePattern2 = /(^|\s)\@(\w*[a-zA-Z_]+\w*)/gim;
-//       replacedText = replacedText.replace(replacePattern2, '$1<a href="https://instagram.com/$2"' + targetAttr + '>@$2</a>');
-//       return replacedText;
-//     };
-//   }
-// ]);
+      var replacedText = $filter('linky')(text, target);
+      var targetAttr = "";
+      if (angular.isDefined(target)) {
+          targetAttr = ' target="' + target + '"';
+      }
+      // replace #hashtags and send them to twitter
+      var replacePattern1 = /(^|\s)#(\w*[a-zA-Z_]+\w*)/gim;
+      replacedText = text.replace(replacePattern1, '$1<a href="https://instagram.com/explore/tags/$2"' + targetAttr + '>#$2</a>');
+      // replace @mentions but keep them to our site
+      var replacePattern2 = /(^|\s)\@(\w*[a-zA-Z_]+\w*)/gim;
+      replacedText = replacedText.replace(replacePattern2, '$1<a href="https://instagram.com/$2"' + targetAttr + '>@$2</a>');
+      return replacedText;
+    };
+  }
+]);
+  
