747e942553b08de17ae8bfbabaf3bc79bc8fd17a
Niraj Vora <nsvora@ucdavis.edu>
2015-10-26 17:12:33 -0700

    started directive for poseidon

--- project/static/js/controllers/bannerController.js
+++ project/static/js/controllers/bannerController.js
@@ -20,4 +20,48 @@ app.controller('BannerController', ['$scope', '$state', 'PostType', 'UsersFactor
       console.log(error);
     });
   };
+}]).directive('poseidon', ['d3Service', function ( d3Service ) {
+  return {
+    restrict: 'EA',
+    scope: {},
+    link: function(scope, element, attrs) {
+      d3Service.d3().then(function(d3) {
+        // d3 code
+        console.log('here');
+
+        var svg = d3.select(element[0])
+                    .append("svg")
+                    .style('width', '100%');
+
+        // Browser onresize event
+        window.onresize = function() {
+          scope.$apply();
+        };
+
+        // hard-code data
+        scope.data = [
+          {name: "Greg", score: 98},
+          {name: "Ari", score: 96},
+          {name: 'Q', score: 75},
+          {name: "Loser", score: 48}
+        ];
+
+        // Watch for resize event
+        scope.$watch(function() {
+          return angular.element($window)[0].innerWidth;
+        }, function() {
+          scope.render(scope.data);
+        });
+
+        scope.render = function(data) {
+          // remove all previous items before render
+          svg.selectAll('*').remove();
+
+          // If we don't pass any data, return out of the element
+          if (!data) return;
+        };
+            
+      });
+    }
+  };
 }]);
