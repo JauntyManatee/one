3a514980cd7b4569d4b1f166904bce8891984cfa
Niraj Vora <nsvora@ucdavis.edu>
2015-10-26 20:00:19 -0700

    appended image to svg background

--- project/static/js/controllers/bannerController.js
+++ project/static/js/controllers/bannerController.js
@@ -27,10 +27,19 @@ app.controller('BannerController', ['$scope', '$state', 'PostType', 'UsersFactor
       scope: {
       },
       link: function(scope, ele, attrs) {
-        console.log('hi');
+      
         var svg = d3.select(ele[0])
           .append('svg')
-          .style('width', '100%');
+          .style({'width': '100%', 'height': '100%'});
+
+        var imgs = svg.selectAll("image").data([0]);
+                imgs.enter()
+                .append("svg:image")
+                .attr("xlink:href", "../static/views/ONE.png")
+                .attr("x", "0")
+                .attr("y", "0")
+                .attr("width", '100%')
+                .attr("height", '795');
 
         // scope.render = function(data) {
         //   svg.selectAll('*').remove();
