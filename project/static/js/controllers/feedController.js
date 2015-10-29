a33b3fbe255812371f71a22b6ef6760ce8565638
Brian Graham <graham.briank@gmail.com>
2015-10-29 14:49:32 -0700

    D3 visuals begun, added NVD3 dependency

--- project/static/js/controllers/feedController.js
+++ project/static/js/controllers/feedController.js
@@ -1,31 +1,33 @@
-app.controller('FeedController', ['$scope', 'TwitterFactory', 'InstagramFactory', 'SoundCloudFactory', 'PostType', '$state', '$sce', '$timeout', 'UsersFactory', 
-  function ( $scope, TwitterFactory, InstagramFactory, SoundCloudFactory, PostType, $state, $sce, $timeout, UsersFactory ) {
+app.controller('FeedController', ['$scope', 'PanelFactory', 'TwitterFactory', 'InstagramFactory', 'SoundCloudFactory', 'PostType', '$location', '$sce', '$timeout', 'UsersFactory', 
+  function ( $scope, PanelFactory, TwitterFactory, InstagramFactory, SoundCloudFactory, PostType, $location, $sce, $timeout, UsersFactory ) {
 
   $scope.feed = [];
 
   $scope.postType = PostType;
 
+  $scope.checked = PanelFactory;   // This will be binded using the ps-open attribute
+
   var buildFeed = function (data, type, date) { 
     var theFeed  = [], 
-        theDate, htmlFrame, obj;
+       theDate, htmlFrame, obj;
     angular.forEach(data, function (item) {
       if (type === 'twitter') {
         obj = {
-          text : item.text, 
-          created_at: new Date(item.created_at),
-          displayTime: moment(new Date(item.created_at)).fromNow(), 
-          type: type, 
-          user: {screen_name : item.user.screen_name}, 
-          id_str: item.id_str 
+         text : item.text, 
+         created_at: new Date(item.created_at),
+         displayTime: moment(new Date(item.created_at)).fromNow(), 
+         type: type, 
+         user: {screen_name : item.user.screen_name}, 
+         id_str: item.id_str 
         };
       } else {
         htmlFrame = $sce.trustAsHtml(item.embed);
         theDate = date ? new Date(item.time * 1000) : new Date(item.time);
         obj = {
-          frame: htmlFrame, 
-          created_at: theDate,
-          displayTime: moment(theDate).fromNow(), 
-          type: type
+         frame: htmlFrame, 
+         created_at: theDate,
+         displayTime: moment(theDate).fromNow(), 
+         type: type
         };
       }
       theFeed.push(obj);
@@ -36,11 +38,10 @@ app.controller('FeedController', ['$scope', 'TwitterFactory', 'InstagramFactory'
   $scope.isValidUser = function () {
     var user = sessionStorage.getItem('at');
     if (!user) {
-      $state.go('home');
+      $location.path('home');
     } else {
       return true;
-    }
-    
+    }    
   };
 
   $scope.getTweets = function ( ) {
@@ -48,11 +49,9 @@ app.controller('FeedController', ['$scope', 'TwitterFactory', 'InstagramFactory'
       if(Array.isArray(data.data)){
         var items = buildFeed(data.data, 'twitter');
         $scope.feed.push.apply($scope.feed, items);
-        console.log($scope.feed);
       }
     });
   };
-
   $scope.favTweet = function ( id ) {
     TwitterFactory.favTweet(id).then(function ( response ) {
       console.log(response);
@@ -85,45 +84,65 @@ app.controller('FeedController', ['$scope', 'TwitterFactory', 'InstagramFactory'
     });
   };
 
-  $scope.checked = false; // This will be binded using the ps-open attribute
-  
-  $scope.toggle = function(){
-      $scope.checked = !$scope.checked;
-  };
-  
-  
+  $scope.postType = PostType;
 
-}])
-.factory('PostType', function ( ) {
-  return {
-    'twitter': false,
-    'instagram': false,
-    'soundcloud': false
+  $scope.toggle = function( type ) {
+    $scope.postType[type] = !$scope.postType[type];
+    return $scope.postType;
   };
-})
-.filter('typeFilter', function ( ) {
-  return function ( input, postTypes ) {
-    var output = [];
 
-    angular.forEach(input, function ( post ) {
-      if ( postTypes[post.type] === false) {
-        if (post.type === 'instagram') {
-          window.instgrm.Embeds.process();
-        }
-        output.push(post);
-      }
+  $scope.panelToggle = function(){
+    PanelFactory.checked = !PanelFactory.checked;
+  };
+
+  $scope.logout = function() {
+    var authToken = sessionStorage.getItem('at');
+    UsersFactory.logout({"at": authToken})
+    .then(function ( res ) {
+      sessionStorage.clear();
+      $location.path('home');
+    })
+    .catch(function ( error ) {
+      console.log(error);
     });
-    return output;
   };
-})
-.directive('poseidon', ['SliderFactory','$window', '$timeout', 'd3Service', 
+
+ }])
+  .factory('PostType', function ( ) {
+    return {
+      'twitter': false,
+      'instagram': false,
+      'soundcloud': false
+    };
+  })
+  .factory('PanelFactory',[function ( ) {
+    return {
+      'checked': false
+    };
+  }])
+  .filter('typeFilter', function ( ) {
+    return function ( input, postTypes ) {
+      var output = [];
+
+      angular.forEach(input, function ( post ) {
+        if ( postTypes[post.type] === false) {
+          if (post.type === 'instagram') {
+            window.instgrm.Embeds.process();
+          }
+          output.push(post);
+        }
+      });
+      return output;
+    };
+  })
+  .directive('poseidon', ['SliderFactory','$window', '$timeout', 'd3Service', 
   function (SliderFactory, $window, $timeout, d3Service) {
     return {
       restrict: 'E',
       scope: {
       },
       link: function(scope, ele, attrs) {
-      
+        var dataset = [];
         var svg = d3.select(ele[0])
           .append('svg')
           .style({'width': '100%', 'height': '100%'});
@@ -131,7 +150,100 @@ app.controller('FeedController', ['$scope', 'TwitterFactory', 'InstagramFactory'
 
         SliderFactory.getFollowStats()
           .then(function(resp){
+            for (var x in resp) {
+              dataset.push({ type: resp[x].media, followers : resp[x].counts.followers, following : resp[x].counts.following });
+            }
+          })
+          .then(function () {
+            nv.addGraph(function() {
+              var chart = nv.models.pieChart()
+                  .x(function(d) { return d.type; })
+                  .y(function(d) { return d.followers; })
+                  .color(['#325C86', '#FF5500', '#54aaec'])
+                  .showLabels(true)     //Display pie labels
+                  .labelThreshold(0.05)  //Configure the minimum slice size for labels to show up
+                  .labelType("percent") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
+                  .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
+                  .donutRatio(0.35);     //Configure how big you want the donut hole size to be.
 
+                d3.select("#pie svg")
+                    .datum(dataset)
+                    .transition().duration(350)
+                    .call(chart);
+
+              return chart;
+            });
+            // var width = 200;
+            // var height = 200;
+            // var radius = Math.min(width, height) / 2;
+
+            // var donutWidth = 15;
+
+            // var legendRectSize = 18;
+            // var legendSpacing = 4;
+
+            // var color = d3.scale.ordinal()
+            //   .range(['#325C86', '#FF5500', '#54aaec']);
+
+            // var svg = d3.select("#pie")
+            //   .append('svg')
+            //   .attr('width', width)
+            //   .attr('height', height)
+            //   .append('g')
+            //   .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+
+            // var arc = d3.svg.arc()
+            //   .innerRadius(radius - donutWidth)
+            //   .outerRadius(radius);
+
+            // var pie = d3.layout.pie()
+            //   .value(function (d) { return d.followers; })
+            //   .sort(null);
+
+            // var path = svg.selectAll('path')
+            //   .data(pie(dataset))
+            //   .enter()
+            //   .append('path')
+            //   .attr('d', arc)
+            //   .attr('fill', function (d, i) {
+            //     return color(d.data.type);
+            //   })
+            //   .on('mouseover', function () {
+            //     d3.select(this).attr('opacity', 0.7);
+            //   })
+            //   .on('mouseleave', function () {
+            //     d3.select(this).attr('opacity', 1);
+            //   })
+            //   .append("svg:title")
+            //   .text(function (d) {
+            //     return d.data.followers;
+            //   });
+              
+
+
+            // var legend = svg.selectAll('.legend')
+            //   .data(color.domain())
+            //   .enter()
+            //   .append('g')
+            //   .attr('class', 'legend')
+            //   .attr('transform', function (d, i) {
+            //     var height = legendRectSize + legendSpacing;
+            //     var offset = height * color.domain().length / 2;
+            //     var horz = -2 * legendRectSize;
+            //     var vert = i * height - offset;
+            //     return 'translate(' + horz + ',' + vert + ')';
+            //   });
+
+            // legend.append('rect')
+            //   .attr('width', legendRectSize)
+            //   .attr('height', legendRectSize)
+            //   .style('fill', color)
+            //   .style('strole', color);
+
+            // legend.append('text')
+            //   .attr('x', legendRectSize + legendSpacing)
+            //   .attr('y', legendRectSize - legendSpacing)
+            //   .text(function (d) {return d; });
           });
 
 
@@ -150,8 +262,4 @@ app.controller('FeedController', ['$scope', 'TwitterFactory', 'InstagramFactory'
 
       }
     };
-}]);
-
-
-
-
+  }]);
